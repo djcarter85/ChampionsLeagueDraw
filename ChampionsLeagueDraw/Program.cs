4be3f0db5d36cad14494d2a345cf104f96c360af
Dan Carter <djcarter85@gmail.com>
2019-03-14 08:59:03 +0000

    Calculate probability

--- ChampionsLeagueDraw/Program.cs
+++ ChampionsLeagueDraw/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace ChampionsLeagueDraw
 {
@@ -6,11 +8,55 @@ namespace ChampionsLeagueDraw
     {
         public static void Main(string[] args)
         {
+            CalculateAndDisplayProbabilityOfAllEnglishMatch();
+        }
+
+        private static void CalculateAndDisplayProbabilityOfAllEnglishMatch()
+        {
+            var totalNumberOfFixtureLists = 1000000;
+
+            Console.WriteLine($"Simulating {totalNumberOfFixtureLists:N0} fixture lists ...");
+
+            var stopwatch = Stopwatch.StartNew();
+
+            var numberOfFixtureListsWithAnAllEnglishMatch = SimulateFixtureLists(totalNumberOfFixtureLists);
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Number with an all-English match: {numberOfFixtureListsWithAnAllEnglishMatch:N0}");
+
+            var probabilityOfAllEnglishMatch = (decimal)numberOfFixtureListsWithAnAllEnglishMatch / totalNumberOfFixtureLists;
+
+            Console.WriteLine($"Probability: {probabilityOfAllEnglishMatch * 100:N4}%");
+
+            Console.WriteLine($"Time taken: {stopwatch.Elapsed.TotalSeconds:N2}s");
+
+            Console.ReadLine();
+        }
+
+        private static int SimulateFixtureLists(int totalNumberOfFixtureLists)
+        {
+            var numberOfFixtureListsWithAnAllEnglishMatch = 0;
+
+            foreach (var index in Enumerable.Range(0, totalNumberOfFixtureLists))
+            {
+                var fixtureList = FixtureList.CreateRandom();
+
+                if (fixtureList.ContainsAllEnglishMatch)
+                {
+                    numberOfFixtureListsWithAnAllEnglishMatch++;
+                }
+            }
+
+            return numberOfFixtureListsWithAnAllEnglishMatch;
+        }
+
+        private static void GenerateAndDisplayFixtureLists()
+        {
             do
             {
                 CreateFixtureListAndDisplay();
-            }
-            while (!string.Equals(Console.ReadLine(), "q", StringComparison.OrdinalIgnoreCase));
+            } while (!string.Equals(Console.ReadLine(), "q", StringComparison.OrdinalIgnoreCase));
         }
 
         private static void CreateFixtureListAndDisplay()
